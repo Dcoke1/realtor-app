c791b390b2cad96287ee61b387369c8e0aaa357d
Douglas Coke <easyg@G-Macbook-Pro.local>
2022-04-15 13:41:28 -0400

    update_config

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["www.ivanandmike.com", "nypost.com", "bayut-production.s3.eu-central-1.amazonaws.com" ]
+    domains: ["www.ivanandmike.com", "nypost.com", "bayut-production.s3.eu-central-1.amazonaws.com", "estate-realtor.herokuapp.com"]
   }
 }
 
