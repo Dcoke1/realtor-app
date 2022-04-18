83a61daf756d17c6858b6d875bf18daa0f8d7679
Douglas Coke <easyg@G-Macbook-Pro.local>
2022-04-18 00:55:46 -0400

    easter_update

--- components/ImageScrollbar.jsx
+++ components/ImageScrollbar.jsx
@@ -5,7 +5,7 @@ import { ScrollMenu, VisibilityContext } from "react-horizontal-scrolling-menu";
 import { FaArrowAltCircleLeft, FaArrowAltCircleRight } from "react-icons/fa";
 
 const LeftArrow = () => {
-  const { scrollPrev } =  useContext(VisibilityContext);
+  const { scrollPrev } = useContext(VisibilityContext);
 
   return (
     <Flex justifyContent="center" alignItems="center" marginRight="1">
@@ -26,7 +26,7 @@ const RightArrow = () => {
     <Flex justifyContent="center" alignItems="center" marginRight="1">
       <Icon
         as={FaArrowAltCircleRight}
-        onClick={()=>scrollNext()}
+        onClick={() => scrollNext()}
         fontSize="2xl"
         cursor="pointer"
       />
@@ -34,31 +34,25 @@ const RightArrow = () => {
   );
 };
 
-export default function ImageScrollbar({ data }) {
-  return (
-    <ScrollMenu
-      LeftArrow={LeftArrow}
-      RightArrow={RightArrow}
-      style={{ overflow: "hidden" }}>
-      {data.map(item => (
-        <Box
-          key={item.id}
-          width="910px"
-          itemID={item.id}
-          overflow="hidden"
-          p="1"
-        >
-          <Image
-            placeholder="blur"
-            blurDataURL={item.url}
-            src={item.url}
-            width={1000}
-            height={500}
-            alt="property"
-            sizes="(max-width:500px) 100px, (max-width):1023px 400px, 1000px"
-          />
-        </Box>
-      ))}
-    </ScrollMenu>
-  );
-}
+const ImageScrollbar = ({ data }) => (
+  <ScrollMenu
+    LeftArrow={LeftArrow}
+    RightArrow={RightArrow}
+    style={{ overflow: "hidden" }}
+  >
+    {data.map((image) => (
+      <Box key={image.id} width="910px" itemID={image.id} overflow="hidden" p="1">
+        <Image
+          placeholder="blur"
+          blurDataURL={image.url}
+          src={image.url}
+          width="1000px"
+          height="500px"
+          alt="property"
+          sizes="(max-width:500px) 100px, (max-width):1023px 400px, 1000px"
+        />
+      </Box>
+    ))}
+  </ScrollMenu>
+);
+export default ImageScrollbar;
